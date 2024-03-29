d335a09e48d5bdcd5f74ff6f65644f0206efd7f6
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-03-29 19:59:51 +0300

    fix docker, add alembic

--- src/config.py
+++ src/config.py
@@ -2,7 +2,11 @@ import os
 
 
 class Config:
-    DATABASE_URI: str = None
+    DB_USER: str = os.getenv("POSTGRES_USER")
+    DB_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    DB_HOST: str = os.getenv("POSTGRES_HOST")
+    DB_NAME: str = os.getenv("POSTGRES_DB")
+    DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"  # noqa: E501
 
 
 def load_config() -> Config:
